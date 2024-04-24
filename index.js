e8fb319abc0434e895012e7fd9bff97a738104d7
vasanth0419 <vasanthsalomonv1904@gmail.com>
2024-04-24 18:12:21 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -12,7 +12,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 
 app.use(cookieParser());
