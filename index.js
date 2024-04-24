7e48cf9bee65d1c3e237dc320cc53e69d32a6c08
vasanth0419 <vasanthsalomonv1904@gmail.com>
2024-04-24 20:34:11 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors"
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -11,7 +11,7 @@ import trendsrouter from "./Router/Trends.router.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-
+app.use(cors());
 
 app.use(morgan("dev"));
 
