650b53da24494d5d790cc991a8686d96356b111a
vasanth0419 <vasanthsalomonv1904@gmail.com>
2024-04-25 11:44:10 +0530

    .

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://womoens-fix.netlify.app",
     credentials: true,
   })
 );
